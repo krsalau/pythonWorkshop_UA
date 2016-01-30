cb0c854313c1a9f75d6ba1a0726d29cd6aa073a7
Pelican <pelican@dhcp-10-132-168-127.uawifi.arizona.edu>
2016-01-30 16:22:38 -0700

    Initial commit of the python files from the UA Software Carpentry

---
+++ conditional.py
@@ -0,0 +1,7 @@
+x = 7
+
+if True:
+    print("We built this city 
+          on love")
+else:
+    print("Oh no, we lied")
---
+++ iteration.py
@@ -0,0 +1,5 @@
+books = ["Tale of two Cities","Harry Potter","Lord of the Rings"]
+nums = range(100)
+
+for num in nums:
+    print(num)
